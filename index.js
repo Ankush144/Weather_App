d29a2dcd4a920a9ffe95590946e9bb12fd85832d
Ankush <ankushpanchal144@gmail.com>
2021-09-10 22:48:37 +0530

    changes file

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
-const fs = require('fs');
-var requests = require('requests');
-const homeFile = fs.readFileSync("home.html","utf-8");
-const http = require('http');
-const { url } = require('inspector');
-const replaceVal = (temp,orgVal)=>{
-    let temperature = temp.replace("{%tempval%}",((orgVal.main.temp)-273.15).toFixed(2));
-    temperature = temperature.replace("{%tempmin%}",((orgVal.main.temp_min)-273.15).toFixed(2));
-    temperature = temperature.replace("{%tempmax%}",((orgVal.main.temp_max)-273.15).toFixed(2));
-    temperature = temperature.replace("{%location%}",orgVal.name);
-    temperature = temperature.replace("{%country%}",orgVal.sys.country);
-    return temperature;
+//modules section
+    const http = require('http');
+    const fs = require('fs');
+    const requests = require("requests");
+ 
+//reading data from the html file
+    const homeFile = fs.readFileSync("home.html","utf-8");
+//defining replaceVal function here
+const replaceVal = (orgVal) => {
+    homeFile.replace("{%tempmax%}",orgVal.main.temp_max);
+    homeFile.replace("{%tempmin%}",orgVal.main.temp_min); 
+    homeFile.replace("{%tempval%}",orgVal.main.temp);
+    homeFile.replace("{%location%}","Banswara");
+    homeFile.replace("{%country%}","India");
 }
-const server =  http.createServer((req,res)=>{ 
-            requests("https://api.openweathermap.org/data/2.5/weather?q=Pune&appid=60ef9914c7d2f33a55349066c0d59cac ")
-        .on('data',(chunk) => {
-            const obj = JSON.parse(chunk);//converting JSON into js object
-            const arr = new Array(obj);
-            //console.log(arr[0].main.temp);//now data will be in the form of array of objects
-            const realTimeData = arr.map( val=> replaceVal(homeFile,val)).join("");
-            res.write(realTimeData);
-            //console.log(realTimeData)
+//creating a server
+    const server = http.createServer();
+//event handling of request of user
+    server.on( "request",( req,res ) => {
+        //using npm pacakage module "request" and geting data from api
+        requests( "https://api.openweathermap.org/data/2.5/weather?q=Banswara&appid=60ef9914c7d2f33a55349066c0d59cac")
+        .on( "data",(data) => { 
+            //converting json file to object 
+            const obj_data = JSON.parse(data);
+            const arr = [obj_data];
+            replaceVal(obj_data);
+            res.end(homeFile);
         })
-        .on('end',(err) => {
-        if (err) return console.log('connection closed due to errors', err);
-        res.end();
+        .on( "end",(err) => {
+            if(err) console.log(err);
+            res.end();
         });
-});
-server.listen(9000,"127.0.0.1",()=>{
-     console.log("starting our server");
-});
+    });
+//responce is done
+    server.listen(9000,"127.0.0.1",()=>{
+        console.log("Working fine")
+    });
+
+//creating this function to set data in the html file
